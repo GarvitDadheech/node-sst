331dafa61583aa5821cf438a7943b15fc5b6239f
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-07-01 11:37:47 +0530

    added /login

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,23 @@
 const http= require('http');
 const server = http.createServer((req,res) => {
+
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html');
-    res.write('<html><head><title>Node JS SST</title></head><body>');
-    res.write('<h1>I AM GAMB8</h1>');
-    res.write('</body></html>');
+
+    // Routing based on request URL
+    if (req.url === '/login') {
+        res.write('<html><head><title>Login</title></head><body>');
+        res.write('<h1>Hello Login</h1>');
+        res.write('</body></html>');
+    } else {
+        res.write('<html><head><title>Node JS SST</title></head><body>');
+        res.write('<h1>I AM GAMB8</h1>');
+        res.write('</body></html>');
+    }
+
     res.end();
 });
+
 const host = 'localhost';
 const port = 3000;
 server.listen(port,host,() => {
