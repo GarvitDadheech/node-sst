745a138100366297ecd25d2a65ae2bfd93da6dd7
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-07-01 09:50:00 +0530

    added more functions of manipulating files and directories

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-function readFileAndPrint(filename) {
+ function readFileAndPrint(filename) {
     fs.readFile(filename,'utf-8',(err,data) => {
         if(err) {
             console.log(`Error reading File: ${err}`);
@@ -8,8 +8,9 @@ function readFileAndPrint(filename) {
         console.log(data);
     });
 }
-function writeExclaimationAtEnd(filename) {
-    fs.appendFile(filename,'!',(err) => {
+
+function writeExclaimationAtEnd(filename,content) {
+    fs.appendFile(filename,content,(err) => {
         if(err) {
             console.log(`Error writing File: ${err}`);
             return;
@@ -17,5 +18,39 @@ function writeExclaimationAtEnd(filename) {
         console.log('File written successfully');
     });
 }
+
+function removeDirectory(directoryName) {
+    fs.rmdir(directoryName,(err) => {
+        if(err) {
+            console.log(`Error removing directory: ${err}`);
+            return;
+        }
+        console.log('Directory removed successfully');
+    });
+}
+
+function renameFile(oldName,newName) {
+    fs.rename(oldName,newName,(err) => {
+        if(err) {
+            console.log(`Error renaming file: ${err}`);
+            return;
+        }
+        console.log('File renamed successfully');
+    });
+}
+
+function makeDirectory(directoryName) {
+    fs.mkdir(directoryName, { recursive: true }, (err) => {
+        if (err) {
+            console.log(`Error creating directory: ${err}`);
+            return;
+        }
+        console.log('Directory created successfully');
+    });
+}
+
 readFileAndPrint('file.txt');
-writeExclaimationAtEnd('file.txt');
+writeExclaimationAtEnd('file.txt','!');
+renameFile('file.txt','newFile.txt');
+makeDirectory('newDirectory');
+removeDirectory('newDirectory');
